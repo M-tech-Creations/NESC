53d510f9d68bb287678cf1077a66f51913d817b8
Mario <mario@mtechcreations.com>
2019-04-20 17:20:02 -0400

    Fixed Issue with define statment
    You must define the Controller type before including the library if you are not going to use the default NES controller

--- examples/Check_Button/Check_Button.ino
+++ examples/Check_Button/Check_Button.ino
@@ -1,11 +1,13 @@
+#define NES_CONTROLLER //This is used to tell the Library which controller you are using
+                       //If you do not define a contoller, the NES controller is used by default
+// The define statment needs to come before the Library inculde to setup the library for the controller
 #include <NESC.h>
 
 /*
  * Example code for checking Buttons with the NESC Libary 4/20/18
  */
 
-#define NES_CONTROLLER //This is used to tell the Library which controller you are using
-                       //If you do not define a contoller, the NES controller is used by default
+
 
 
 NESC controller1; //Create the NESC object to be used in the rest of the program
